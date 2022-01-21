c8debea68f5097693350b83dac34d0d5e8e1ef84
galaxyx <88942854+galaxyxone@users.noreply.github.com>
2022-01-21 17:36:40 -0600

    everything is fixed, git commit this

---
+++ src/Wallet.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+class Wallet extends Component {
+  state = {
+    message: "",
+  };
+
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+      profile: {},
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    this.loadUserProfile();
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, error) =>
+      this.setState({ profile, error })
+    );
+  }
+  render() {
+    const { DataisLoaded, profile } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>{" "}
+        </div>
+      );
+
+    return (
+      <div className="Wallet">
+        <h1> My Certificates of Authenticity </h1>{" "}
+        <h3>
+          Address:
+          {(profile &&
+            profile.app_metadata &&
+            profile.app_metadata.walletaddr) ||
+            ""}
+        </h3>
+        
+              </div>
+    );
+  }
+}
+
+export default Wallet;
